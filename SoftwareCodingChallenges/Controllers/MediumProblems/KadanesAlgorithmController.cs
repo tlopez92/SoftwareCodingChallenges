dedeb3f95f236e7c0d7da414b391a16afcac2df2
tlopez92 <wtjlopez99@gmail.com>
2023-02-08 01:16:43 -0500

    Swagger is default page and Kadanes Algorithm problem added

---
+++ SoftwareCodingChallenges/Controllers/MediumProblems/KadanesAlgorithmController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCodingChallenges.Controllers.MediumProblems
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KadanesAlgorithmController : BaseApiController
+    {
+        /*
+         * Problem Statement:
+         * Given an array of integers, find the contiguous subarray with the largest sum.
+         * 
+         * Example:
+         * Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+         * Output: 6
+         * Explanation: [4, -1, 2, 1] has the largest sum = 6
+         * 
+         * Input: [1, 2, 3, 4, 5]
+         * Output: 15
+         * Explanation: [1, 2, 3, 4, 5] has the largest sum = 15
+         * 
+         * Input: [-1, -2, -3, -4, -5]
+         * Output: -1
+         * Explanation: [-1] has the largest sum = -1
+         * 
+         * Input: [1, 2, 3, -4, 5]
+         * Output: 9
+         * Explanation: [1, 2, 3, -4, 5] has the largest sum = 9
+         * 
+         * Input: [1, 2, 3, -4, 5, -6, 7]
+         * Output: 8
+         * Explanation: [1, 2, 3, -4, 5, -6, 7] has the largest sum = 8
+         * 
+         * Input: [1, 2, 3, -4, 5, -6, 7, -8, 9]
+         * Output: 9
+         * Explanation: [1, 2, 3, -4, 5, -6, 7, -8, 9] has the largest sum = 9
+         * 
+         * Input: [1, 2, 3, -4, 5, -6, 7, -8, 9, -10, 11]
+         * Output: 11
+         * Explanation: [1, 2, 3, -4, 5, -6, 7, -8, 9, -10, 11] has the largest sum = 11
+         */
+        [HttpPost("/kadanes_algorithm/")]
+        public IActionResult ChangeDirectory([FromBody]int[] input)
+        {
+            int maxSoFar = input[0];
+            int maxEndingHere = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                maxEndingHere = Math.Max(input[i], maxEndingHere + input[i]);
+                maxSoFar = Math.Max(maxSoFar, maxEndingHere);
+            }
+
+            return Ok(maxSoFar);
+        }
+    }
+}
--- SoftwareCodingChallenges/Program.cs
+++ SoftwareCodingChallenges/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,22 +22,18 @@ builder.Services.AddSwaggerGen(
         });
     });
 
+
+
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 app.UseSwagger();
-app.UseSwaggerUI();
-
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Easy Challenges");
+    c.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
